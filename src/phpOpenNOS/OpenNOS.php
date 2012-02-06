149a43108754f6ee9eb6c46db50a70211cc0d762
Stefan Koopmanschap <left@leftontheweb.com>
2012-02-06 15:49:59 +0200

    fixing some standard issues

--- src/phpOpenNOS/OpenNOS.php
+++ src/phpOpenNOS/OpenNOS.php
@@ -105,7 +105,8 @@ class OpenNOS
      */
     public function search($query, $sort = self::SORT_SCORE)
     {
-        $url = 'http://open.nos.nl/v2/search/query/key/'.$this->apikey.'/output/xml/q/'.urlencode($query).'/sort/'.$sort;
+        $query = urlencode($query);
+        $url = 'http://open.nos.nl/v2/search/query/key/'.$this->apikey.'/output/xml/q/'.$query.'/sort/'.$sort;
         $documents = array();
 
         $xml = $this->request($url);
