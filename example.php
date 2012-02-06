8b05ee5356716bd16c0d7d4fec8017766b61d58b
Stefan Koopmanschap <left@leftontheweb.com>
2012-02-06 09:36:18 +0200

    updated example

--- example.php
+++ example.php
@@ -2,7 +2,7 @@
 
 require_once 'src/phpOpenNOS/OpenNOS.php';
 
-$nos = new phpOpenNOS\OpenNOS('API-key');
+$nos = new phpOpenNOS\OpenNOS('de3dedac0350bd6a26b4c187e0d27b69');
 
 //$articles = $nos->getLatestArticles();
 //var_dump($articles);
@@ -13,11 +13,11 @@ $nos = new phpOpenNOS\OpenNOS('API-key');
 //$audios = $nos->getLatestAudio(phpOpenNos\OpenNOS::SPORT);
 //var_dump($audios);
 
-//$documents = $nos->search('Nederland');
-//var_dump($documents);
+$documents = $nos->search('Nederland', phpOpenNOS\OpenNOS::SORT_DATE);
+var_dump($documents);
 
 //$tv = $nos->getTvBroadcast('2010-12-15', '2010-12-20', 'NL1');
 //var_dump($tv);
 
-$radio = $nos->getRadioBroadcast('2010-12-01', '2010-12-03');
-var_dump($radio);
+//$radio = $nos->getRadioBroadcast('2010-12-01', '2010-12-03');
+//var_dump($radio);
